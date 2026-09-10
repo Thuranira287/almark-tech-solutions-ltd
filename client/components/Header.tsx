b459ef2b93404f5d14be81bfcd6e2d460bca97a7
Alexander Thuranira <147224695+Thuranira287@users.noreply.github.com>
2026-09-10 17:32:40 +0300

    Update Header.tsx

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -7,7 +7,7 @@ import { SiGmail } from "react-icons/si";
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const handleEmailClick =() => {
-    window.location.href = "mailto:info@almarktech.com";
+    window.location.href = "mailto:info@almarktech.co.ke";
   };
 
   const navLinks = [
